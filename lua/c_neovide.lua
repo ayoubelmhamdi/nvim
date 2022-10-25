72b05ad0fb3480b1021adf87c6f4857c43353159
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-25 03:10:14 +0100

    up

--- lua/c_neovide.lua
+++ lua/c_neovide.lua
@@ -4,6 +4,6 @@ if exists("g:neovide")
     let g:neovide_cursor_animation_length=0.02
     let g:neovide_cursor_antialiasing=v:true
     let g:neovide_cursor_vfx_mode = "ripple"
-    set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h10
+    set guifont=Fisa\ Code:h20
 endif
 ]]
