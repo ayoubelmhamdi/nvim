cff3e642c9659f7d249a9cdef12e2df187e49c5b
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-25 03:10:09 +0100

    up

--- lua/c_lspsaga.lua
+++ lua/c_lspsaga.lua
@@ -10,7 +10,7 @@ saga.init_lsp_saga()
 keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
 
 -- Code action
-keymap({ 'n', 'v' }, '<space>ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
+keymap({ 'n', 'v' }, ',ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
 
 -- Rename
 keymap('n', '<space>rn', '<cmd>Lspsaga rename<CR>', { silent = true })
