81cf726a7b9a0f10faa75c45883f6570374959ae
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-25 03:11:18 +0100

    use some plugin in dir start

--- init.lua
+++ init.lua
@@ -1,48 +1,57 @@
-local vim_req = function(name)
+VIM_REQ = function(name)
   local nvim = vim.fn.stdpath 'config' .. '/lua/'
   vim.cmd('source ' .. nvim .. name .. '.vim')
 end
+VIM_REQ 'c_startup'
+-- lsp+cmp(deps like luasnip) not work with (loop and packadd)
 
-vim_req 'c_startup'
+-- not work coorrectly with vim.loop
+-- require 'c_lspconfig'
 
-vim.loop.new_timer():start(
-  1,
-  0,
-  vim.schedule_wrap(function()
-    local opt = vim.fn.stdpath 'data' .. '/site/pack/packer/opt/'
-    for dir in io.popen('ls ' .. opt):lines() do
-      vim.cmd('packadd ' .. dir)
-    end
-    -- octave overwrite by theme, and make error with treesitter
-    vim_req 'c_theme'
-    require 'c_theme'
-    -- third part vimL plugin
-    vim_req 'c_gdiff'
-    vim_req 'c_octave'
-    -- lua config
-    -- lspconfig not loaded with async, need reopen file again
-    require 'c_lspkind'
-    require 'c_lspsaga'
-    require 'c_null-ls'
-    require 'c_lspconfig'
-    require 'c_cmp'
-    require 'c_treesitter'
-    require 'c_devicons'
-    require 'c_indent-blankline'
-    require 'c_lualine'
-    require 'c_nvim-tree'
-    require 'c_telescope'
-    require 'c_autopair'
-    require 'c_leap'
-    require 'c_surround'
-    require 'c_comment'
-    require 'c_gitsigns'
-    require 'c_hlslens'
-    require 'c_overseer'
-    require 'c_set-setting'
-    require 'c_neovide'
-    require 'c_keymapings'
-    require 'c_autocmd'
-    require 'c_function'
-  end)
-)
+vim.defer_fn(function()
+  local opt = vim.fn.stdpath 'data' .. '/site/pack/packer/opt/'
+  for dir in io.popen('ls ' .. opt):lines() do
+    vim.cmd('packadd ' .. dir)
+  end
+
+  --
+  -- cmp
+  -- it suppert load config inside `vim.loop`
+  -- but must add to the `rtp` after `vim.loop`
+  --
+  require 'c_lspconfig'
+  vim.cmd [[LspStart]]
+
+  require 'c_cmp'
+
+  VIM_REQ 'c_gdiff'
+  VIM_REQ 'c_octave'
+
+  VIM_REQ 'c_theme'
+  require 'c_theme'
+
+  --
+  -- third part vimL plugin
+  --
+  require 'c_lspsaga'
+  require 'c_lspkind'
+  require 'c_null-ls'
+  require 'c_treesitter'
+  require 'c_devicons'
+  require 'c_indent-blankline'
+  require 'c_lualine'
+  require 'c_nvim-tree'
+  require 'c_telescope'
+  require 'c_autopair'
+  require 'c_leap'
+  require 'c_surround'
+  require 'c_comment'
+  require 'c_gitsigns'
+  require 'c_hlslens'
+  require 'c_overseer'
+  require 'c_set-setting'
+  require 'c_neovide'
+  require 'c_keymapings'
+  require 'c_autocmd'
+  require 'c_function'
+end, 2)
