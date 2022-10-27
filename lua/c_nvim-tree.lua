b71782e4f3c82916115529b81a0f79c2830154ee
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-27 12:19:47 +0100

    up

--- lua/c_nvim-tree.lua
+++ lua/c_nvim-tree.lua
@@ -1,14 +1,14 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
+local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
-  print('nvim-tree unavailable')
+  print 'nvim-tree unavailable'
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
 if not config_status_ok then
-  print('nvim-tree.config unavailable')
+  print 'nvim-tree.config unavailable'
   return
 end
 
@@ -16,42 +16,42 @@ nvim_tree.setup {
   open_on_setup = true,
   actions = {
     open_file = {
-      quit_on_open = true
-    }
+      quit_on_open = true,
+    },
   },
   renderer = {
     icons = {
       glyphs = {
-        default = "",
-        symlink = "",
+        default = '',
+        symlink = '',
         git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
+          unstaged = '',
+          staged = 'S',
+          unmerged = '',
+          renamed = '➜',
+          deleted = '',
+          untracked = 'U',
+          ignored = '◌',
         },
         folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        }
-      }
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+        },
+      },
     },
-    root_folder_modifier = ":t",
+    root_folder_modifier = ':t',
   },
   diagnostics = {
     enable = true,
     show_on_dirs = true,
     icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+      hint = '',
+      info = '',
+      warning = '',
+      error = '',
     },
   },
 }
