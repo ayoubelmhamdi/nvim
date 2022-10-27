d97aa55bb141e676a69829c3307163e8633fe80e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-27 12:19:43 +0100

    up

--- lua/c_set-setting.lua
+++ lua/c_set-setting.lua
@@ -9,22 +9,11 @@ vim.o.splitright = true
 --vim.wo.signcolumn = "yes"
 --vim.o.clipboard = "unnamedplus"
 vim.cmd [[
-" fold
-" set foldmethod=marker
-" set fillchars=fold:\ 
 
-"set scrolloff=99
-"set sidescrolloff=99
-
-setlocal! spell! spelllang=en,fr
-
-set spellsuggest+=10
+set spellsuggest+=5
 set linebreak
-set path+=**
-set whichwrap=b,s,<,>,h,l,[,]
 set nojoinspaces 
 set linespace=5
-" -- Make substitution work in realtime
 set clipboard+=unnamedplus
 set clipboard+=unnamed
 set inccommand=split 
@@ -41,8 +30,6 @@ set expandtab
 set smarttab
 
 "set backspace=indent,eol,start
-" set colorcolumn=110
-"set cursorline
 
 set mouse=a
 set updatetime=300
@@ -58,10 +45,7 @@ set undodir=/tmp/nvim/backup3//
 
 set autoread autowrite
 set ignorecase smartcase
-" set list listchars=eol:⏎
 
-" bug here
-" set cmdheight=1
 
 set nohlsearch
 augroup vimrc-incsearch-highlight
@@ -69,55 +53,4 @@ augroup vimrc-incsearch-highlight
   autocmd CmdlineEnter /,\? :set hlsearch
 augroup END
 
-" Disabled Now: 
-"     set foldmethod=manual
-"     set formatprg=prettier
-"     set incsearch
-"     set linebreak
-"     set nohlsearch
-"     set showtabline=2
-
-
-
-""""set complete+=kspell
-""""set formatoptions=cro
-""""set go=a
-""""set laststatus=2
-""""set matchpairs+=<:> "TODO: use filetype 
-""""set mmp=5000
-""""set modelines=2
-""""set noerrorbells visualbell t_vb=
-""""set noshiftround
-""""set nospell
-""""set nostartofline
-""""set pumheight=10
-""""set regexpengine=1
-""""set ruler
-""""set shortmess+=c
-""""set showcmd
-""""set showmatch
-""""set showmode
-""""set spelllang=en_us
-""""set timeoutlen=900
-""""set ttimeout
-""""set ttimeoutlen=0
-""""set wildmenu
-""""set wildmode=longest,list,full
-""""set virtualedit=block
-
-
-
-
-"""""tow line for schow error
-""""" set endofline
-""""" set list
-""""" set listchars=tab:>-,eol:¶
-""""" set listchars=tab:>.,trail:.,precedes:<,extends:>
-""""" set tw=0
-""""" set wm=0
-""""" using only 1 column (and 1 space) while possible
-""""" set numberwidth=1
-""""" set nolazyredraw
-
-" set textwidth=60
 ]]
