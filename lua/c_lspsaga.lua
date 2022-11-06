445cda8f7be87314b08a0dca425bba68e948c47e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-11-06 08:39:01 +0100

    up

--- lua/c_lspsaga.lua
+++ lua/c_lspsaga.lua
@@ -3,32 +3,84 @@ local saga = require 'lspsaga'
 
 saga.init_lsp_saga {
   border_style = 'rounded',
+  symbol_in_winbar = {
+    in_custom = true,
+  },
 }
 
--- Lsp finder find the symbol definition implement reference
--- if there is no implement it will hide
--- when you use action in finder like open vsplit then you can
--- use <C-t> to jump back
-keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
+-- local function config_winbar()
+--   local exclude = {
+--     ['terminal'] = true,
+--     ['toggleterm'] = true,
+--     ['prompt'] = true,
+--     ['NvimTree'] = true,
+--     ['help'] = true,
+--   } -- Ignore float windows and exclude filetype
+--   if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
+--     vim.wo.winbar = ''
+--   else
+--     local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
+--     local sym
+--     if ok then
+--       sym = lspsaga.get_symbol_node()
+--     end
+--     if sym ~= nil then
+--       sym = ' '
+--     end
+--     vim.wo.winbar = sym
+--   end
+-- end
+local function config_winbar_or_statusline()
+    local exclude = {
+        ['terminal'] = true,
+        ['toggleterm'] = true,
+        ['prompt'] = true,
+        ['NvimTree'] = true,
+        ['help'] = true,
+    } -- Ignore float windows and exclude filetype
+    if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
+        vim.wo.winbar = ''
+    else
+        local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
+        local sym
+        if ok then sym = lspsaga.get_symbol_node() end
+        local win_val = ''
+        win_val = ' ' -- always set space for winbar
+        if sym ~= nil then win_val = win_val .. sym end
+        vim.wo.winbar = win_val
+    end
+end
 
--- Code action
+
+local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
+
+vim.api.nvim_create_autocmd(events, {
+    pattern = '*',
+    callback = function() config_winbar_or_statusline() end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'LspsagaUpdateSymbol',
+    callback = function() config_winbar_or_statusline() end,
+})
+
 keymap({ 'n', 'v' }, ',ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
-
--- Rename
 keymap('n', '<space>rn', '<cmd>Lspsaga rename<CR>', { silent = true })
-
--- Peek Definition
--- you can edit the definition file in this flaotwindow
--- also support open/vsplit/etc operation check definition_action_keys
--- support tagstack C-t jump back
 keymap('n', 'gd', '<cmd>Lspsaga peek_definition<CR>', { silent = true })
+
+
+keymap('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { silent = true })
+keymap('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { silent = true })
+keymap('n', '<space>o', '<cmd>LSoutlineToggle<CR>', { silent = true })
+keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
+keymap('n', '<A-d>', '<cmd>Lspsaga open_floaterm<CR>', { silent = true })
+keymap('n', '<A-d>', '<cmd>Lspsaga open_floaterm lazygit<CR>', { silent = true })
+keymap('t', '<A-d>', [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
 
 -- Show line diagnostics
 keymap('n', '<space>cd', '<cmd>Lspsaga show_line_diagnostics<CR>', { silent = true })
 
--- Diagnsotic jump can use `<c-o>` to jump back
-keymap('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { silent = true })
-keymap('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { silent = true })
+keymap('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
 
 -- Only jump to error
 keymap('n', '[e', function()
@@ -37,17 +89,3 @@ end, { silent = true })
 keymap('n', ']e', function()
   require('lspsaga.diagnostic').goto_next { severity = vim.diagnostic.severity.ERROR }
 end, { silent = true })
-
--- Outline
-keymap('n', '<space>o', '<cmd>LSoutlineToggle<CR>', { silent = true })
-
--- Hover Doc
-keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
-
--- Float terminal
-keymap('n', '<A-d>', '<cmd>Lspsaga open_floaterm<CR>', { silent = true })
--- if you want pass somc cli command into terminal you can do like this
--- open lazygit in lspsaga float terminal
-keymap('n', '<A-d>', '<cmd>Lspsaga open_floaterm lazygit<CR>', { silent = true })
--- close floaterm
-keymap('t', '<A-d>', [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
