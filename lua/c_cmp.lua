b0813695ca40ebbca9762403c842de7ec9a8ec91
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-11-26 13:54:46 +0100

    use tabnine sort

--- lua/c_cmp.lua
+++ lua/c_cmp.lua
@@ -291,3 +291,21 @@ require('cmp_tabnine.config'):setup {
   },
   show_prediction_strength = true,
 }
+
+local compare = require('cmp.config.compare')
+cmp.setup({
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require('cmp_tabnine.compare'),
+      compare.offset,
+      compare.exact,
+      compare.score,
+      compare.recently_used,
+      compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+    },
+  },
+})
