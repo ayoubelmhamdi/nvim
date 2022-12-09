6c0b9d5a2e9cfc9517fb2a7b648249a206d2dd22
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-12-09 23:42:25 +0100

    disable onenerd

--- lua/c_theme.lua
+++ lua/c_theme.lua
@@ -1,66 +1,67 @@
-local colors = require('onenord.colors').load()
-require('onenord').setup {
-  theme = dark, -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
-  borders = true, -- Split window borders
-  -- Style that is applied to various groups: see `highlight-args` for options
-  styles = {
-    comments = 'italic,bold',
-    strings = 'bold',
-    keywords = 'NONE',
-    functions = 'bold',
-    variables = 'NONE',
-    diagnostics = 'NONE',
-  },
-  disable = {
-    background = false, -- Disable setting the background color
-    cursorline = false, -- Disable the cursorline
-    eob_lines = true, -- Hide the end-of-buffer lines
-  },
-  -- Inverse highlight for different groups
-  inverse = {
-    match_paren = true,
-  },
-  custom_highlights = {
-
-    -- NvimTree
-    NvimTreeOpenedFile = { fg = colors.diff_add, style = 'bold' },
-
-    -- Local Syntax
-    Conditional = { fg = colors.blue },
-    Number = { fg = colors.purple },
-    Operator = { fg = colors.dark_blue },
-    Statement = { fg = colors.fg },
-    MatchParen = { fg = colors.light_purple, bg = colors.none, style = 'bold' },
-
-    -- TreeSitter
-    TSBoolean = { fg = colors.yellow },
-    TSConstructor = { fg = colors.fg },
-    TSConditional = { fg = colors.blue },
-    TSConstBuiltin = { fg = colors.light_green },
-    TSConstMacro = { fg = colors.light_green },
-    TSFloat = { fg = colors.purple },
-    TSFunction = { fg = colors.cyan },
-    TSKeyword = { fg = colors.blue },
-    TSKeywordReturn = { fg = colors.blue },
-    TSKeywordFunction = { fg = colors.cyan },
-    TSNumber = { fg = colors.purple },
-    TSOperator = { fg = colors.blue },
-    TSParameter = { fg = colors.fg },
-    TSPunctBracket = { fg = colors.cyan },
-    TSPunctDelimiter = { fg = colors.cyan },
-    TSStringEscape = { fg = colors.purple },
-    TSType = { fg = colors.blue },
-    -- TSField = { fg = colors.red, bg = colors.yellow },
-
-    LspReferenceText = { style = 'NONE', bg = colors.cyan },
-    LspReferenceRead = { style = 'NONE', bg = colors.cyan },
-    LspReferenceWrite = { style = 'NONE', bg = colors.cyan },
-
-    --ayoub
-    SpellBad = { fg = colors.none, bg = colors.none, style = 'italic,underline', sp = colors.red },
-  }, -- Overwrite default highlight groups
-  custom_colors = {}, -- Overwrite default colors
-}
+vim.cmd 'colorscheme gruvbox-material'
+-- local colors = require('onenord.colors').load()
+-- require('onenord').setup {
+--   theme = dark, -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+--   borders = true, -- Split window borders
+--   -- Style that is applied to various groups: see `highlight-args` for options
+--   styles = {
+--     comments = 'italic,bold',
+--     strings = 'bold',
+--     keywords = 'NONE',
+--     functions = 'bold',
+--     variables = 'NONE',
+--     diagnostics = 'NONE',
+--   },
+--   disable = {
+--     background = false, -- Disable setting the background color
+--     cursorline = false, -- Disable the cursorline
+--     eob_lines = true, -- Hide the end-of-buffer lines
+--   },
+--   -- Inverse highlight for different groups
+--   inverse = {
+--     match_paren = true,
+--   },
+--   custom_highlights = {
+--
+--     -- NvimTree
+--     NvimTreeOpenedFile = { fg = colors.diff_add, style = 'bold' },
+--
+--     -- Local Syntax
+--     Conditional = { fg = colors.blue },
+--     Number = { fg = colors.purple },
+--     Operator = { fg = colors.dark_blue },
+--     Statement = { fg = colors.fg },
+--     MatchParen = { fg = colors.light_purple, bg = colors.none, style = 'bold' },
+--
+--     -- TreeSitter
+--     TSBoolean = { fg = colors.yellow },
+--     TSConstructor = { fg = colors.fg },
+--     TSConditional = { fg = colors.blue },
+--     TSConstBuiltin = { fg = colors.light_green },
+--     TSConstMacro = { fg = colors.light_green },
+--     TSFloat = { fg = colors.purple },
+--     TSFunction = { fg = colors.cyan },
+--     TSKeyword = { fg = colors.blue },
+--     TSKeywordReturn = { fg = colors.blue },
+--     TSKeywordFunction = { fg = colors.cyan },
+--     TSNumber = { fg = colors.purple },
+--     TSOperator = { fg = colors.blue },
+--     TSParameter = { fg = colors.fg },
+--     TSPunctBracket = { fg = colors.cyan },
+--     TSPunctDelimiter = { fg = colors.cyan },
+--     TSStringEscape = { fg = colors.purple },
+--     TSType = { fg = colors.blue },
+--     -- TSField = { fg = colors.red, bg = colors.yellow },
+--
+--     LspReferenceText = { style = 'NONE', bg = colors.cyan },
+--     LspReferenceRead = { style = 'NONE', bg = colors.cyan },
+--     LspReferenceWrite = { style = 'NONE', bg = colors.cyan },
+--
+--     --ayoub
+--     SpellBad = { fg = colors.none, bg = colors.none, style = 'italic,underline', sp = colors.red },
+--   }, -- Overwrite default highlight groups
+--   custom_colors = {}, -- Overwrite default colors
+-- }
 
 -- vim.loop.new_timer():start(
 --   9000,
