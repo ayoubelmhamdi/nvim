9bd819f50736537f2aede27c4e3c11c9f27e44d9
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-12-09 23:42:05 +0100

    up

--- lua/c_packer.lua
+++ lua/c_packer.lua
@@ -53,25 +53,27 @@ packer.startup(function(use)
   -- ██      ██      ██    ██ ██    ██ ██ ██  ██ ██
   -- ██      ███████  ██████   ██████  ██ ██   ████
 
-  use { 'wbthomason/packer.nvim', opt = true }
+  use { 'wbthomason/packer.nvim', opt = false }
 
   use { 'lewis6991/impatient.nvim' }
   use { 'dstein64/vim-startuptime' }
   use { 'nvim-lua/plenary.nvim' }
 
-  use { 'kyazdani42/nvim-tree.lua', opt = true }
-
+  -- use { 'kyazdani42/nvim-tree.lua' }
+  use 'https://github.com/oberblastmeister/zoom.nvim'
+  use '/data/projects/lua/vf.nvim'
   -- ████████ ██   ██ ███████ ███    ███ ███████
   --    ██    ██   ██ ██      ████  ████ ██
   --    ██    ███████ █████   ██ ████ ██ █████
   --    ██    ██   ██ ██      ██  ██  ██ ██
   --    ██    ██   ██ ███████ ██      ██ ███████
   --
-  use { 'rmehri01/onenord.nvim', opt = true }
-  use { 'sainnhe/gruvbox-material', opt = true }
-  use { 'kyazdani42/nvim-web-devicons', opt = true }
-  use { 'lukas-reineke/indent-blankline.nvim', opt = true }
-  use { 'nvim-lualine/lualine.nvim', opt = true }
+  use { 'https://github.com/roflolilolmao/oceanic-next.nvim', opt = false }
+  use { 'rmehri01/onenord.nvim', opt = false }
+  use { 'sainnhe/gruvbox-material', opt = false }
+  use { 'kyazdani42/nvim-web-devicons', opt = false }
+  use { 'lukas-reineke/indent-blankline.nvim', opt = false }
+  use { 'nvim-lualine/lualine.nvim', opt = false }
   use { 'b0o/incline.nvim' }
 
   --@ use {
@@ -91,14 +93,14 @@ packer.startup(function(use)
   --    ██    ██   ██ ██      ██                 ██ ██    ██       ██    ██      ██   ██
   --    ██    ██   ██ ███████ ███████       ███████ ██    ██       ██    ███████ ██   ██
 
-  use { 'nvim-treesitter/nvim-treesitter', opt = true }
-  use { 'nvim-treesitter/playground', opt = true }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects', opt = true }
-  use { 'p00f/nvim-ts-rainbow', opt = true }
-  use { 'JoosepAlviste/nvim-ts-context-commentstring', opt = true }
-  use { 'David-Kunz/treesitter-unit', opt = true }
-  use { 'lewis6991/spellsitter.nvim', opt = true }
-  use { 'https://github.com/cshuaimin/ssr.nvim', opt = true }
+  use { 'nvim-treesitter/nvim-treesitter', opt = false }
+  use { 'nvim-treesitter/playground', opt = false }
+  use { 'nvim-treesitter/nvim-treesitter-textobjects', opt = false }
+  use { 'p00f/nvim-ts-rainbow', opt = false }
+  use { 'JoosepAlviste/nvim-ts-context-commentstring', opt = false }
+  use { 'David-Kunz/treesitter-unit', opt = false }
+  use { 'lewis6991/spellsitter.nvim', opt = false }
+  use { 'https://github.com/cshuaimin/ssr.nvim', opt = false }
 
   -- ████████ ███████ ██      ███████ ███████  ██████  ██████  ██████  ███████
   --    ██    ██      ██      ██      ██      ██      ██    ██ ██   ██ ██
@@ -106,9 +108,9 @@ packer.startup(function(use)
   --    ██    ██      ██      ██           ██ ██      ██    ██ ██      ██
   --    ██    ███████ ███████ ███████ ███████  ██████  ██████  ██      ███████
 
-  use { 'nvim-telescope/telescope.nvim', opt = true }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', opt = true }
-  use { 'ThePrimeagen/harpoon', opt = true }
+  use { 'nvim-telescope/telescope.nvim', opt = false }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', opt = false }
+  use { 'ThePrimeagen/harpoon', opt = false }
 
   -- ██      ███████ ██████
   -- ██      ██      ██   ██
@@ -116,20 +118,20 @@ packer.startup(function(use)
   -- ██           ██ ██
   -- ███████ ███████ ██
 
-  use { 'neovim/nvim-lspconfig', opt = true }
-  use { 'jose-elias-alvarez/null-ls.nvim', opt = true }
+  use { 'neovim/nvim-lspconfig', opt = false }
+  use { 'jose-elias-alvarez/null-ls.nvim', opt = false }
 
-  -- use { 'kosayoda/nvim-lightbulb', opt = true }
-  use { 'SmiteshP/nvim-navic', opt = true }
-  use { 'nvim-lua/lsp-status.nvim', opt = true }
-  use { 'onsails/lspkind-nvim', opt = true }
-  use { 'glepnir/lspsaga.nvim', opt = true }
-  use { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim', opt = true }
-  use { 'windwp/nvim-ts-autotag', opt = true }
-  -- use { 'ray-x/lsp_signature.nvim', opt = true }
+  -- use { 'kosayoda/nvim-lightbulb', opt = false }
+  use { 'SmiteshP/nvim-navic', opt = false }
+  use { 'nvim-lua/lsp-status.nvim', opt = false }
+  use { 'onsails/lspkind-nvim', opt = false }
+  use { 'glepnir/lspsaga.nvim', opt = false }
+  use { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim', opt = false }
+  use { 'windwp/nvim-ts-autotag', opt = false }
+  -- use { 'ray-x/lsp_signature.nvim', opt = false }
 
-  use { 'folke/neodev.nvim', opt = true }
-  use { 'akinsho/flutter-tools.nvim', opt = true }
+  use { 'folke/neodev.nvim', opt = false }
+  use { 'akinsho/flutter-tools.nvim', opt = false }
 
   --  ██████ ███    ███ ██████
   -- ██      ████  ████ ██   ██
@@ -137,8 +139,8 @@ packer.startup(function(use)
   -- ██      ██  ██  ██ ██
   --  ██████ ██      ██ ██
 
-  use { 'rafamadriz/friendly-snippets', opt = true }
-  use { 'L3MON4D3/LuaSnip', tag = 'v<CurrentMajor>.*', opt = true }
+  use { 'rafamadriz/friendly-snippets', opt = false }
+  use { 'L3MON4D3/LuaSnip', opt = false }
   use { 'hrsh7th/nvim-cmp' }
   use { 'saadparwaiz1/cmp_luasnip' }
   use { 'hrsh7th/cmp-nvim-lua' }
@@ -162,10 +164,10 @@ packer.startup(function(use)
   -- ██   ██ ██   ██ ██
   -- ██████  ██   ██ ██
 
-  use { 'mfussenegger/nvim-dap', opt = true }
-  use { 'rcarriga/nvim-dap-ui', opt = true }
-  use { 'theHamsta/nvim-dap-virtual-text', opt = true }
-  use { 'leoluz/nvim-dap-go', opt = true }
+  use { 'mfussenegger/nvim-dap', opt = false }
+  use { 'rcarriga/nvim-dap-ui', opt = false }
+  use { 'theHamsta/nvim-dap-virtual-text', opt = false }
+  use { 'leoluz/nvim-dap-go', opt = false }
 
   -- ███    ██ ███████ ██     ██
   -- ████   ██ ██      ██     ██
@@ -173,30 +175,30 @@ packer.startup(function(use)
   -- ██  ██ ██ ██      ██ ███ ██
   -- ██   ████ ███████  ███ ███
 
-  use { 'ray-x/guihua.lua', run = 'cd lua/fzy && make', opt = true }
-  use { 'ggandor/leap.nvim', opt = true }
-  use { 'kylechui/nvim-surround', opt = true }
-  use { 'numToStr/Comment.nvim', opt = true }
-  use { 'kevinhwang91/nvim-hlslens', opt = true }
+  use { 'ray-x/guihua.lua', run = 'cd lua/fzy && make', opt = false }
+  use { 'ggandor/leap.nvim', opt = false }
+  use { 'kylechui/nvim-surround', opt = false }
+  use { 'numToStr/Comment.nvim', opt = false }
+  use { 'kevinhwang91/nvim-hlslens', opt = false }
 
   --  -- git
   --  use { 'sindrets/diffview.nvim' }
   --  use { 'TimUntersberger/neogit' }
-  use { 'airblade/vim-rooter', opt = true }
-  use { 'lewis6991/gitsigns.nvim', opt = true }
+  use { 'airblade/vim-rooter', opt = false }
+  use { 'lewis6991/gitsigns.nvim', opt = false }
 
   --  -- Misc
-  use { '907th/vim-auto-save', opt = true }
+  use { '907th/vim-auto-save', opt = false }
   --  use { 'tjdevries/train.nvim' }
 
   --  -- Explorer & Terminal
   --  use { 'luukvbaal/nnn.nvim' }
   --  use { 'voldikss/vim-floaterm' }
-  use { 'static-nvim/mkdir', opt = true }
-  use { 'fedepujol/move.nvim', opt = true }
-  use { 'WhoIsSethDaniel/lualine-lsp-progress.nvim', opt = true }
-  use { 'barreiroleo/ltex-extra.nvim', opt = true }
-  use { 'stevearc/overseer.nvim', opt = true }
+  use { 'static-nvim/mkdir', opt = false }
+  use { 'fedepujol/move.nvim', opt = false }
+  use { 'WhoIsSethDaniel/lualine-lsp-progress.nvim', opt = false }
+  use { 'barreiroleo/ltex-extra.nvim', opt = false }
+  use { 'stevearc/overseer.nvim', opt = false }
 end)
 
 -- vim.cmd [[autocmd BufWritePost packer.lua source <afile> | PackerCompile]]
