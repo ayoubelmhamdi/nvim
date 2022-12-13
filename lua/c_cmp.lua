b69822422f13444ae570ec0b9fe2b2f0034dc196
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-12-13 13:09:51 +0100

    up

--- lua/c_cmp.lua
+++ lua/c_cmp.lua
@@ -279,33 +279,33 @@ end)
 
 --vim.keymap.set('i', '<c-u>', require 'luasnip.extras.select_choice')
 
-require('cmp_tabnine.config'):setup {
-  max_lines = 40,
-  max_num_results = 2,
-  sort = true,
-  run_on_every_keystroke = true,
-  snippet_placeholder = '..',
-  ignored_file_types = { -- default is not to ignore
-    -- uncomment to ignore in lua:
-    -- lua = true
-  },
-  show_prediction_strength = true,
-}
+-- require('cmp_tabnine.config'):setup {
+--   max_lines = 40,
+--   max_num_results = 2,
+--   sort = true,
+--   run_on_every_keystroke = true,
+--   snippet_placeholder = '..',
+--   ignored_file_types = { -- default is not to ignore
+--     -- uncomment to ignore in lua:
+--     -- lua = true
+--   },
+--   show_prediction_strength = true,
+-- }
 
-local compare = require('cmp.config.compare')
-cmp.setup({
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      require('cmp_tabnine.compare'),
-      compare.offset,
-      compare.exact,
-      compare.score,
-      compare.recently_used,
-      compare.kind,
-      compare.sort_text,
-      compare.length,
-      compare.order,
-    },
-  },
-})
+-- local compare = require('cmp.config.compare')
+-- cmp.setup({
+--   sorting = {
+--     priority_weight = 2,
+--     comparators = {
+--       require('cmp_tabnine.compare'),
+--       compare.offset,
+--       compare.exact,
+--       compare.score,
+--       compare.recently_used,
+--       compare.kind,
+--       compare.sort_text,
+--       compare.length,
+--       compare.order,
+--     },
+--   },
+-- })
